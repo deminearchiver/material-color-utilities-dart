df64f14b07f40b0e33e6f955ae0be160e0e14848
DeMineArchiver <minearchiver@gmail.com>
2025-07-05 08:18:57 +0300

    Initial commit

---
+++ packages/material_color_utilities/example/material_color_utilities_example.dart
@@ -0,0 +1,6 @@
+import 'package:material_color_utilities/material_color_utilities.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/material_color_utilities/lib/material_color_utilities.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/material_color_utilities_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
